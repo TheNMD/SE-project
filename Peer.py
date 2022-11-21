f20ae9a9d51901d722a6ec2892ac8957c5f9d06e
DaRealHius <78369139+HiamBucky@users.noreply.github.com>
2022-11-21 09:35:12 +0700

    Delete Peer.py

--- Peer.py
+++
@@ -1,6 +0,0 @@
-class Peer:
-    def __init__(self, name, port, peer_socket, status = True) -> None:
-        self.name = name
-        self.port = port
-        self.peer_socket = peer_socket
-        self.status = status
