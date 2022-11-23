c32c643d80455a0863bcf78857418d81b868bd01
Đan Nguyễn <78247196+TheNMD@users.noreply.github.com>
2022-11-23 21:28:37 +0700

    Add files via upload

--- Task 5/direction.py
+++ Task 5/direction.py
@@ -1,24 +1,49 @@
-import googlemaps, json, os, fnmatch, numpy
+import googlemaps, json, os, fnmatch
 
 map = googlemaps.Client(key="AIzaSyCT9dShISsjp0qie6WvIMUAEuSCEPlG1vo")
 counter = len(fnmatch.filter(os.listdir("./result"), '*.json*'))
 
 # Calculating shortest path
-coord_list = []
+id_list, coord_list = [], []
 with open("./data/mcp.json", "r") as mcp_file:
     mcp_list = json.load(mcp_file)
     for mcp in mcp_list["mcp_list"]:
+        id_list.append(mcp['mcp_id'])
         coord_list.append(mcp['mcp_coord'])
         
-direction = map.directions(origin=coord_list[0], destination=coord_list[0], waypoints=coord_list[1:], optimize_waypoints = True, mode='driving')
-with open(f"./result/Route{counter}.json", "w") as route_file:
-    route_file.write(json.dumps(direction, indent=4))
-center_lat = (direction[0]["bounds"]["northeast"]["lat"] + direction[0]["bounds"]["southwest"]["lat"]) / 2
+direction = map.directions(origin=coord_list[0], destination=coord_list[0], waypoints=coord_list, optimize_waypoints = True, mode='driving')
+center_lat = (direction[0]["bounds"]["northeast"]["lat"] + direction[0]["bounds"]["southwest"]["lat"]) / 2 # Find center of 2 bounds
 center_lng = (direction[0]["bounds"]["northeast"]["lng"] + direction[0]["bounds"]["southwest"]["lng"]) / 2
 
+#Write into json file
+with open(f"./result/R{counter}.json", "w") as route_file:
+    route_file.write(json.dumps(direction, indent=4))
+    
+with open("./data/route.json", "r+") as route_file:
+    route_list = json.load(route_file)
+    route_id = len(route_list["route_list"])
+    route_ord = direction[0]["waypoint_order"]
+    route_direction = "M0 -> "
+    for idx in route_ord:
+        route_direction += id_list[int(idx)] + " -> "
+    route_direction = route_direction[:-4]
+    route_length, route_time = 0, 0
+    for idx in direction[0]["legs"]:
+        route_length += idx["distance"]["value"]
+        route_time += idx["duration"]["value"]
+    new_route = {
+                    "route_id": f"R{route_id}",
+                    "route_direction": route_direction,
+                    "route_length": f"{route_length} m",
+                    "route_time": f"{route_time} mins",
+                    "route_status": "Unassigned"
+                }
+    route_list["route_list"].append(new_route)
+    route_file.seek(0)
+    json.dump(route_list, route_file, indent = 4)
+    
 # Plot map as an image
-marker_points = []
-waypoints = []
+marker_points, waypoints = [], []
 for leg in direction[0]["legs"]:
     leg_start_loc = leg["start_location"]
     marker_points.append(f'{leg_start_loc["lat"]},{leg_start_loc["lng"]}')
@@ -29,38 +54,18 @@ last_stop = direction[0]["legs"][-1]["end_location"]
 marker_points.append(f'{last_stop["lat"]},{last_stop["lng"]}')
 markers = [ "color:blue|size:mid|label:" + chr(65+i) + "|" + r for i, r in enumerate(marker_points)]
 map_direction = map.static_map (center = f"{center_lat},{center_lng}",
-                             scale=2, 
-                             zoom=13,
-                             size=[1024, 1024], 
-                             format="jpg", 
-                             maptype="roadmap",
-                             markers=markers,
-                             path="color:0x0000ee|weight:2|" + "|".join(waypoints))
+                                scale=2, 
+                                zoom=13,
+                                size=[1024, 1024], 
+                                format="jpg", 
+                                maptype="roadmap",
+                                markers=markers,
+                                path="color:0x0000ee|weight:2|" + "|".join(waypoints))
 
-with open(f"./Result/Route{counter}.jpg", "wb") as img:
+with open(f"./Result/R{counter}.jpg", "wb") as img:
     for chunk in map_direction:
         img.write(chunk)
-  
-# Write route as a string        
-route = []
-with open("./data/mcp.json") as mcp_file:
-    mcp_list = json.load(mcp_file)
-    for point in marker_points:
-            for mcp in mcp_list["mcp_list"]:
-                if(point == mcp["mcp_coord"]):
-                    route.append(mcp["mcp_id"])
 
-with open("./data/route.json", "r+") as route_file:
-    route_list = json.load(route_file)
-    route_id = len(route_list["route_list"])
-    route_direction = " -> ".join(route)
-    new_route = {
-                    "route_id": f"{route_id}",
-                    "route_direction": f"{route_direction}",
-                    "route_status": "Unassigned",
-                }
-    route_list["route_list"].append(new_route)
-    route_file.seek(0)
-    json.dump(route_list, route_file, indent = 4)
+
     
     
